d6454a1f49aa942023c2d64ebc6db7b63698f81d
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-10-14 07:05:55 +0200

    started paging

---
+++ ModelsBuilderified/App_Code/PagingExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for PagingExtensions
+/// </summary>
+public class PagingExtensions
+{
+    public PagingExtensions()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
